7b7fa2a8191b3c74ca4faaa025d9d8cc89f7438a
Lyphaa <BOUboulover23@>
2020-04-20 09:54:09 +0200

    Avec Liste

--- app/src/main/java/com/example/application_1/MainActivity.java
+++ app/src/main/java/com/example/application_1/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.application_1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private List_adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        // use this setting to
+        // improve performance if you know that changes
+        // in content do not change the layout size
+        // of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        List<String> input = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            input.add("Soirée " + i);
+        }
+        // define an adapter
+        mAdapter = new List_adapter(input);
+        recyclerView.setAdapter(mAdapter);
     }
+
 }
+
